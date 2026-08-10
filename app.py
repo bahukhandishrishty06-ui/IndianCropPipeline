b372c3f84fd775d2666e366772aa51027534d424
bahukhandishrishty06-ui <bahukhandishrishty06-ui@users.noreply.github.com>
2026-08-10 09:40:12 +0530

    integrate model prediction into app

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-from flask import Flask, render_template, request, jsonify
+# Model prediction initialization
+import joblib
 import pandas as pd
-import os
-
-app = Flask(__name__, static_folder='static')
-DATASET_PATH = 'India_Districts_Crop_Production_Processed.csv'
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
